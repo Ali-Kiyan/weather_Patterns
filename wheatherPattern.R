6aa554d2d4c2708645a681b9e781741385d9f639
Ali <alikiyand@gmail.com>
2017-11-14 22:51:45 +0000

    adding lapply

--- wheatherPattern.R
+++ wheatherPattern.R
@@ -16,7 +16,7 @@ Weather <- list(Chicago=Chicago, NewYork=NewYork,Houston=Houston, SanFrancisco=S
 #tapply for vectors
 #by used on dataframes same concept as group by in SQL
 #eapply used in an environment
-#lapply is used on lists
+#lapply is used on lists or vector 
 #sapply like lists but more simpler and its is result is eaither in vector or marix
 #vapply prespecified return value 
 #replicate runs a function several time and usually used with generation of random variables
@@ -36,3 +36,13 @@ names(output) <- rownames(Chicago)
 output 
 #2.via apply function
 apply(Chicago, 1, mean)
+Chicago
+t(Chicago)
+#apply transpose function on every component of a list 
+tWeather <- lapply(Weather, t)
+tWeather
+rbind(Chicago, NewRow=1:12)
+lapply(Weather, rbind, NewRow=1:12)
+rowMeans(Chicago) #identical to : apply(Chicago,1, mean)
+lapply(Weather, rowMeans)
+?lapply      
