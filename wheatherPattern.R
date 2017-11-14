1a20034cada4402abf13252acf6764ecd9bcdbe5
Ali <alikiyand@gmail.com>
2017-11-14 22:27:09 +0000

    adding apply function

--- wheatherPattern.R
+++ wheatherPattern.R
@@ -10,3 +10,29 @@ Chicago <- as.matrix(Chicago)
 NewYork <- as.matrix(NewYork)
 Houston <- as.matrix(Houston)
 SanFrancisco <- as.matrix(SanFrancisco)
+#Creating a list of 4 components
+Weather <- list(Chicago=Chicago, NewYork=NewYork,Houston=Houston, SanFrancisco=SanFrancisco)
+#apply only works on matrices
+#tapply for vectors
+#by used on dataframes same concept as group by in SQL
+#eapply used in an environment
+#lapply is used on lists
+#sapply like lists but more simpler and its is result is eaither in vector or marix
+#vapply prespecified return value 
+#replicate runs a function several time and usually used with generation of random variables
+Chicago
+apply(Chicago, 1, mean)
+apply(NewYork, 1, mean)
+apply(Houston, 1, mean)
+apply(SanFrancisco, 1, mean)
+#recreating apply function with for loop
+#find the mean for every row
+#1.via loops
+output <- NULL #preparing an empty vector 
+for(i in 1:5){  #run the cycle
+  output[i] <- mean(Chicago[i,])
+}
+names(output) <- rownames(Chicago)
+output 
+#2.via apply function
+apply(Chicago, 1, mean)
